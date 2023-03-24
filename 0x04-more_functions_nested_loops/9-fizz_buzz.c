96ace5bb1184b54713d07425b06e2968c6b740d8
Sudireri <sudi.m@hotmail.com>
2023-03-24 05:57:56 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,12 +20,13 @@ int main(void)
 		{
 			printf(" Buzz");
 		}
-		if else (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf(" FizzBuzz");
 		}
 		else if (i == 1)
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
 		else
 		{
