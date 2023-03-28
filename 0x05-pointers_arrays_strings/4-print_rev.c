bc74eea5797c155c048895d35fb5f3f4262e7cec
Sudireri <sudi.m@hotmail.com>
2023-03-28 12:12:37 -0700

    Reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@
 
 void print_rev(char *s)
 {
-	int i;
-	int count = 0;
+	int i = 0;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-		count++;
-	for (i = count ; i >= 0 ; i--)
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
+
 	_putchar('\n');
 }
