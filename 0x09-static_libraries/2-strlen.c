c358b90177c858853e1049a9e3a2267bf2ebb97c
Sudireri <sudi.m@hotmail.com>
2023-04-08 01:43:21 -0700

    A library is not a luxury

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenth of a string
+ *
+ * @s: string whose length to be printed
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: string parameter
+ * @accept: prefix to be measured
+ *
+ * Return: returns the number of bytes in the initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+		else if (accept[i + 1] == '\0')
+			return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
