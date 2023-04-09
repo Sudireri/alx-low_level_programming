061700536cf22e766adfce944b5fd9c31d33ca0a
Sudireri <sudi.m@hotmail.com>
2023-04-09 08:03:03 -0700

    Minimal Number of Coins for Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - name of the program
+ * @argc: counts the number of arguments
+ * @argv: memory storing the arguments
+ *
+ * Return: 1 if arguments not exactly 1, otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents = cents - 25;
+		else if (cents >= 10)
+			cents = cents - 10;
+		else if (cents >= 5)
+			cents = cents - 5;
+		else if (cents >= 2)
+			cents = cents - 2;
+		else if (cents >= 1)
+			cents = cents - 1;
+		coins++;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
