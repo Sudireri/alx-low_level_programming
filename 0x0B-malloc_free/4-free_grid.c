41d73f09f4dbfbd4c5e3fac2628f0c973a784296
Sudireri <sudi.m@hotmail.com>
2023-04-11 12:50:53 -0700

    If you even dream

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return (0);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
