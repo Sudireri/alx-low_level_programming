f58d2529df1ca800c80114c502440e683aca4320
Sudireri <sudi.m@hotmail.com>
2023-04-12 01:52:29 -0700

    He who is not courageous enough

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * *str_concat - function that concatenates two strings
  * @s1: first string to concatenate
  * @s2: second string to concatenate
  *
- * Return: s1 and s2
+ * Return: returns a pointer or NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	char *p;
+	int i;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-	if (conct == NULL)
+	}
+	i = strlen(s1) + strlen(s2) + 1;
+	p = (char *) malloc(i * sizeof(char));
+	if (p == NULL)
+	{
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++;
 	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	strcpy(p, s1);
+	strcat(p, s2);
+	return (p);
 }
