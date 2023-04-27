189fdcee43881c6bd806c9fcc0f09bb2b42b4130
Sudireri <sudi.m@hotmail.com>
2023-04-27 13:00:57 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * printBefore - function that prints the string
+ *
+ * Return: nothing
+ */
+
+void __attribute__ ((constructor)) printBefore()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
