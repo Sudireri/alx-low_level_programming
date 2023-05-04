8cf6d44eefd52a02b03022ac67dd37319d1ce586
Sudireri <sudi.m@hotmail.com>
2023-05-04 12:24:06 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+
+	if ((i >> 31) == 1)
+		return (0);
+	else
+		return (1);
+}
